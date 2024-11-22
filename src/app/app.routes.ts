9833551e5c686af5e36a7cc3639ec8337ec6c312
SannidhiBhatRobosoft <sannidhi.bhat@robosoftin.com>
2024-11-22 12:11:05 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,11 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
 import { AboutComponent } from './about/about.component';
-
-export const routes: Routes = [
-    {
-        path:'/About',component:AboutComponent
-=======
 import { ServiceComponent } from './service/service.component';
 
 export const routes: Routes = [
     {
-        path:'/service',component:ServiceComponent
->>>>>>> 31857070874263f2108d3f38709bb62a4c72e4d4
+        path:'/About',component:AboutComponent
+    },{
+        path:'/service',component:ServiceComponent,
     }
 ];

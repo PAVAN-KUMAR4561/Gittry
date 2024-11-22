31857070874263f2108d3f38709bb62a4c72e4d4
SannidhiBhatRobosoft <sannidhi.bhat@robosoftin.com>
2024-11-22 10:51:13 +0530

    Created Services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ServiceComponent } from './service/service.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'/service',component:ServiceComponent
+    }
+];
